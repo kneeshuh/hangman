1f48269fc8a9fdad872a91efa9ba2313fa210ec4
kneeshuh <ni.gettings@gmail.com>
2024-05-22 15:21:13 +0100

    create test word component - visible when guessed letters are in the word, hidden when not

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,7 +1,28 @@
 import React from 'react'
 
 export default function HangmanWord() {
+    const word = 'test'
+    const guessedLetters = ['t', 'e', 'g']
   return (
-    <div>HangmanWord</div>
+    <div style={{
+        display: 'flex',
+        gap: '.25em',
+        fontSize: '6rem',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontFamily: 'monospace'
+    }}>
+        {word.split('').map((letter, index) => (
+            <span key={index} style={{
+                borderBottom: '.1em solid black'
+            }}>
+                <span style={{
+                    visibility: guessedLetters.includes(letter) ? 'visible' : 'hidden'
+                }}>
+                    {letter}
+                </span>
+            </span>
+        ))}
+    </div>
   )
 }
